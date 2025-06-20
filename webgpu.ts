12c33484a5c0edebcbfe90846766261f61ed8604
Aleksandr Shoronov <alexshoronov@gmail.com>
2025-06-20 12:45:27 +0300

    WebGPU Tutorial. Chapter 6

--- webgpu.ts
+++ webgpu.ts
@@ -1,3 +1,5 @@
+const GRID_SIZE = 32;
+
 const canvas = document.querySelector("canvas")!;
 
 if (!navigator.gpu) {
@@ -28,6 +30,118 @@ context.configure({
   format: canvasFormat,
 });
 
+const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
+
+const uniformBuffer = device.createBuffer({
+  label: "Grid Uniforms",
+  size: uniformArray.byteLength,
+  usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
+
+device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
+
+// prettier-ignore
+const vertices = new Float32Array([
+  -0.8, -0.8,
+  0.8, -0.8,
+  0.8, 0.8,
+
+  -0.8, -0.8,
+  0.8, 0.8,
+  -0.8, 0.8
+]);
+
+const vertexBuffer = device.createBuffer({
+  label: "Cell vertices",
+  size: vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+
+device.queue.writeBuffer(vertexBuffer, 0, vertices);
+
+const vertexBufferLayout: GPUVertexBufferLayout = {
+  arrayStride: 8,
+  attributes: [
+    {
+      format: "float32x2",
+      offset: 0,
+      shaderLocation: 0,
+    },
+  ],
+};
+
+const cellShaderModule = device.createShaderModule({
+  label: "Cell Shader",
+  code: `
+    @group(0) @binding(0) var<uniform> grid: vec2f;
+
+    struct VertexInput {
+      @location(0) pos: vec2f,
+      @builtin(instance_index) instance: u32
+    }
+
+    struct VertexOutput {
+      @builtin(position) pos: vec4f,
+      @location(0) cell: vec2f
+    }
+
+    @vertex
+    fn vertexMain(input: VertexInput) -> VertexOutput {
+      let i = f32(input.instance);
+      let cell = vec2f(i % grid.x, floor(i / grid.y));
+      let cellOffset = cell / grid * 2;
+      let gridPos = (input.pos + 1) / grid - 1 + cellOffset;
+
+      var output: VertexOutput;
+
+      output.pos = vec4f(gridPos, 0, 1);
+      output.cell = cell;
+
+      return output;
+    }
+
+    struct FragInput {
+      @location(0) cell: vec2f
+    }
+
+    @fragment
+    fn fragmentMain(input: FragInput) -> @location(0) vec4f {
+      let c = input.cell / grid;
+      return vec4f(c, 1 - c.x, 1);
+    }
+  `,
+});
+
+const cellPipeline = device.createRenderPipeline({
+  label: "Cell Pipeline",
+  layout: "auto",
+  vertex: {
+    module: cellShaderModule,
+    entryPoint: "vertexMain",
+    buffers: [vertexBufferLayout],
+  },
+  fragment: {
+    module: cellShaderModule,
+    entryPoint: "fragmentMain",
+    targets: [
+      {
+        format: canvasFormat,
+      },
+    ],
+  },
+});
+
+const bindGroup = device.createBindGroup({
+  label: "Cell Renderer Bind Group",
+  layout: cellPipeline.getBindGroupLayout(0),
+  entries: [
+    {
+      binding: 0,
+      resource: { buffer: uniformBuffer },
+    },
+  ],
+});
+
 const encoder = device.createCommandEncoder();
 
 const pass = encoder.beginRenderPass({
@@ -35,16 +149,15 @@ const pass = encoder.beginRenderPass({
     {
       view: context.getCurrentTexture().createView(),
       loadOp: "clear",
-      clearValue: {
-        r: 1,
-        g: 0,
-        b: 0,
-        a: 1,
-      },
       storeOp: "store",
     },
   ],
 });
+
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.setBindGroup(0, bindGroup);
+pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE);
 
 pass.end();
 
